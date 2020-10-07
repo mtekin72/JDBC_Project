10b9c4527ec3265f0e495a1718f172961a85778d
Mustafa Tekin <mtekin72@gmail.com>
2020-10-07 18:31:32 +0200

    deneme mesajı

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,9 @@
+package add_file_here;
+
+import oracle.jdbc.internal.XSCacheOutput;
+
+public class EU3_Practice {
+    public static void main(String[] args) {
+        System.out.println("EU3 is opening pull some changes");
+    }
+}
